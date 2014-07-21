bc3f15b050559770264e70a3967b15091cd84318
laroja <linus.roja@gmail.com>
2014-07-21 16:51:50 +0200

    oj

--- python/test/test_flask-working.py
+++ python/test/test_flask-working.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/user/<username>')
@@ -10,6 +11,25 @@ def show_user_profile(username):
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post {}'.format(post_id)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
 
 if __name__ == '__main__':
 	app.run()
