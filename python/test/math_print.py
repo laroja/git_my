f3a131c48269cb1b8f10a0ae5eb7538726456dd1
laroja <linus.roja@gmail.com>
2014-08-06 22:46:53 +0200

    2

---
+++ python/test/math_print.py
@@ -0,0 +1,10 @@
+l1 = [print (2 + 4),print (6 - 4),
+          print (6 * 3),print (6 / 3),
+          print (6 % 3),print (6 // 3),
+          print (-5),print (3**2)]
+
+if __name__ == ('__main__'):
+    for output in l1:
+        pass
+
+
