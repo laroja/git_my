1dcbca3d1fb88a86de20412605c9cc13e023cb51
laroja <linus.roja@gmail.com>
2014-08-22 00:57:51 +0200

    teamklass

--- python/bs4/fpl/teamsklass.py
+++ python/bs4/fpl/teamsklass.py
@@ -4,43 +4,51 @@ import csv
 
 class club:
     #20 teams
-    #name, fixture list: dict{team: 'xxx', '1': '[date, opposition, H/A], etc}
+    #name, fixture list: dict{'1': '[date, opposition, H/A], etc}
     #Init, skapa ett default lag
     def __init__(self):
     	self.club_name = 'default'
-    	self.fixtures_dict = get_20_dictValues()
-
-
+    	self.fixtures_dict = get_38_dictValues()
 
     #Genererar en dict på 20 värden
-    def get_20_dictValues(self):
+    def get_38_dictValues(self):
     	pass
 
     #Main, specificerar det tidigare default laget.
-    def main(self):
+    def main(self, teamname, j_teamData):
     	#Ange lagets namn. Ange hela fixture dicten.
-    	pass
+    	self.club_name = teamname
+        #build fixtures_list
+        for key in self.fixtures_dict.keys():
+            self.fixtures_dict[key] = j_teamData[key]
 
+    #write to file, name + kyes in fixtures_dict
+    def prnt(self, csvfile):
+        c_list = [self.club_name]
+        for key in self.fixtures_dict.keys():
+            c_list.append(key)
+        with open(csvfile, 'a') as fout:
+            wr = csv.writer(fout, quoting=csv.QUOTE_ALL)
+            wr.writerow(c_list)
 
+        d_list = [self.club_name]
+        for value in self.fixtures_dict.values():
+            d_list.append(value)
+        with open(csvfile, 'a') as fout:
+            wr = csv.writer(fout, quoting=csv.QUOTE_ALL)
+            wr.writerow(d_list)
 
 if __name__ == ('__main__'):
 	#Gå igenom alla lag sidorna. Bygg object för varje lag. Placera objekt i dict. Output the dict to a .csv file
-	teams_dict = {}
+	
 	for teamId in range(1, 21):
 		newclub = club()	#initiate a new club
-
 		res = requests.get('lag.html')	#get lagets html
 		j_teamData = json.loads(res.text) #bygg ett jsonObject
-
 		teamname = j_teamData.get['TeamValue...............']	#Ange lagets DictName
-		teams_dict[teamname] = newclub.main() 	#bygg ett element i dict med club.main med j_teamData
-
-	#Skriv ut teams_dict till en .csv, där varje rad motsvarar ett clubObject.
-
-		
-	
-
-
+        newclub.main(teamname, j_teamData)
+        newclub.prnt(csvfile)
+   
 #request team page
 #json object on request
 #Make empty list. Fill dict {json.'teamname': classObject
