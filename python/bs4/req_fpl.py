47d20aa1981eef489f70ab103942f78946b343e1
unknown <linus.roja@gmail.com>
2014-08-21 16:56:47 +0200

    jjj

--- python/bs4/req_fpl.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-response = requests.get('http://fantasy.premierleague.com/transfers/')
--- python/bs4/req_fpl2.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-re =  requests.get('http://fantasy.premierleague.com/web/api/elements/1/')
-
-
-print('hej')
-
